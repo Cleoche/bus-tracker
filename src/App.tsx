8f50d56855aa51dd16dacfabb2feec4d695ea164
Cleo Thor <kiszo.r6@gmail.com>
2025-05-23 12:48:25 -0400

    removed nyc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Uhhhhh from "./components/Uhhhhh.tsx";
-import Uploader from "./components/Uploader.tsx";
 
 function App() {
 
   return (
     <>
         <Uhhhhh/>
-        <Uploader/>
     </>
   )
 }
